d7f941bf9abff546665015dd58029bb0f07b8dda
handsome <893003171@qq.com>
2022-02-12 14:55:19 +0800

    修复错误

--- firmware/Src/drivers/bsp_uarts.c
+++ firmware/Src/drivers/bsp_uarts.c
@@ -22,7 +22,7 @@ void HAL_UART_RxHalfCpltCallback(UART_HandleTypeDef *huart)
     }
     else if (huart->Instance == USART1)
     {
-        bsp_uart1_dma_tx_cplt_cb();
+        bsp_uart1_dma_rx_halt_cb();
     }
 }
 
@@ -34,6 +34,6 @@ void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart)
     }
     else if (huart->Instance == USART1)
     {
-        bsp_uart1_dma_tx_cplt_cb();
+        bsp_uart1_dma_rx_cplt_cb();
     }
 }
